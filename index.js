a06b4c74014aa42470075ef58bac5749caba70a0
github username <github email address>
2019-01-05 15:33:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your code in this file!
+const scuberGreetingForFeet = (feet) => {
+    if (feet < 400) {
+        return "This one is on me!";
+    } else if (feet > 2000 && feet < 2500){
+        return 'I will gladly take your thirty bucks.';
+    } else if (feet > 2500) {
+        return 'No can do.';
+    } else {
+        return 'some cab fare';
+    }
+};
+
+
+const ternaryCheckCity = (destination) => {
+    return destination === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+}
+
+
+const switchOnCharmFromTip = (tip) => {
+    switch (tip) {
+        case 'generous':
+            return  "Thank you so much.";
+        case 'not as generous':
+            return  "Thank you.";
+        default:
+            return  "Bye.";
+        }    
+}

4f24291f8b61facdae192d8df376dc2879a90c31
jodyneckles <jody@technative.tv>
2019-01-06 11:33:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your code in this file!
+function scuberGreetingForFeet(distInFeet) {
+  let greeting;
+  if (distInFeet >= 0 && distInFeet <= 400) {
+    greeting = "This one is on me!";
+  } else if (distInFeet > 2000 && distInFeet <= 2500) {
+    greeting = "I will gladly take your thirty bucks.";
+  } else {
+    greeting = "No can do.";
+  }
+  return greeting
+}
+
+function ternaryCheckCity(city) {
+  return (city === "NYC" ? "Ok, sounds good." : "No go.");
+}
+
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.';
+  }
+}
